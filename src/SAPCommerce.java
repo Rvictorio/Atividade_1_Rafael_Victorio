7a6313d75aab8fbcdff05652df3d8a3b342b089e
Rafael Victorio <rvictorio8@gmail.com>
2023-01-16 10:52:00 -0300

    Atividade 1

---
+++ src/SAPCommerce.java
@@ -0,0 +1,8 @@
+public class SAPCommerce {
+    public void addToCard(){
+        System.out.print("Adicionando ao carrinho");
+    }
+    public void removeItemfromCard(){
+        System.out.print("Removendo do carrinho");
+    }
+}
---
+++ src/TesteSAPCommerce.java
@@ -0,0 +1,7 @@
+public class TesteSAPCommerce {
+    public static void main(String [] args){
+        SAPCommerce sapcommerce = new SAPCommerce();
+        sapcommerce.addToCard();
+        sapcommerce.removeToCard();
+    }
+}
